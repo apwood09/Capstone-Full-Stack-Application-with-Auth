bb3c5e7fbcbb62d75a66078e8e22b2b76048fa91
abbi portwood <apwood09@gmail.com>
2026-06-19 10:20:23 -0400

    project file struture

---
+++ server/models.py
@@ -0,0 +1,37 @@
+# db.Model: classes recognized by SQLAlchemy as db tables 
+# SerializerMixin: helper class, easy to convert SQLAlchemy model instances into dictionaries (return JSON from Flask API)
+# nullable=False: db constraint ensures a record cannot be saved unless specific fields have a value; cannot be empty OR null 
+
+from flask_sqlalchemy from SQLAlchemy 
+from flask_bcrypt from Bcrypt 
+from sqlalchemy_serializer import SerializerMixin 
+
+# initialize extensions 
+db = SQLAlchemy() # db interaction 
+Bcrypt = Bcrypt() # secure password hashing 
+
+# CLASSES 
+# User 
+class User(db.Model, SerializerMixin): 
+    __tablename__ = 'users'
+
+    # primary key 
+    id = db.Column(db.Integer, primary_key=True)
+
+    # User credentials
+    username = db.Column(db.String, unique=True, nullable=False)
+    # store hashed password 
+    password_hash = db.Column(db.String, nullable=False)
+
+class Asset(db.Model, SerializerMixin): 
+    __tablename__= 'assets'
+
+    # primary key 
+    id = db.Column(db.Integer, primary_key=True)
+
+    # asset detials 
+    name = db.Column(db.String, nullable=False)
+
+    # foreign key: one-to-many relationship
+    # one user -> many assets
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
