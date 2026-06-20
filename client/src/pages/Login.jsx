0a7474972378468e9d01c70d209314a785f799da
abbi portwood <apwood09@gmail.com>
2026-06-20 10:59:19 -0400

    initial commit of Login Page

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,50 @@
+// primary user interaction point for authentication flow 
+
+import { useState, useContext } from "react";
+import { AuthContext } from '../context/AuthContext'; 
+import { useNavigate } from 'react-router-dom'
+
+const Login = () => {
+    // local state: track input fileds username & password
+    const [formData, setFormData] = useState({ username: '', password: ''}); 
+
+    // import login function -> AuthContext handle authentication 
+    const { login } = useContext(AuthContext); 
+
+    // react router hook: redirect user after sucessful login
+    const navigate = useNavigate(); 
+
+    // handle form submission logic 
+    const handleSubmit = async (e) => {
+        // prevent broswer refreshing page on submit 
+        e.preventDefault(); 
+        try {
+            // attempt login: calls backend -> AuthContext
+            await login(formData.username, formData.password); 
+            // successful login: redirect user -> dashbaord 
+            navigate('/dashbaord'); 
+        } catch (err) {
+            // login fails: send alert message below 
+            alert('Login failed. Please check your credentials.');
+        }
+    }; 
+
+    return (
+        <form onSubmit={handleSubmit} className="p-8">
+            {/* username input */}
+            <input 
+                onChange={(e) => setFormData({...formData, username: e.target.value})}
+                className="border p-2 block w-full mb-4" placeholder="Username" 
+            />
+            {/* password input */}
+            <input 
+                onChange={(e) => setFormData({...formData, password: e.target.value})}
+                type="password" className="border p-2 block w-full mb-4" placeholder="Password" 
+            />
+            {/* submission button */}
+            <button className="bg-blue-500 text-white p-2 w-full">Login</button>
+        </form>
+    );
+}; 
+
+export default Login; 
