3826939216fbed7bae3b1df28aef5289539e8e9e
abbi portwood <apwood09@gmail.com>
2026-06-22 09:35:43 -0400

    inital commit MVP

---
+++ server/routes/auth.py
@@ -0,0 +1,25 @@
+# handles creation of new user & JWT tokens
+
+from flask import Blueprint, request, jsonify
+from models import db, User, bcrypt
+from flask_jwt_extended import create_access_token
+
+auth_bp = Blueprint('auth_bp', __name__)
+
+@auth_bp.route('/signup', methods=['POST'])
+def signup(): 
+    data = request.json
+    hashed_pw = bcrypt.generate_password_hash(data['password']).decode('utf-8')
+    new_user = User(username=data['username', password_hash=hashed_pw])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"message": "User created successfuly"}), 201
+
+@auth_bp.route('/login', mmethods=['POST'])
+def login(): 
+    data = request.json
+    user = User.query.filter_by(username=data['username']).first()
+    if user and bcrypt.check_password_hash(user.password_hash, data['password']): 
+        token = create_access_token(identity=user.id)
+        return jsonify(access_token=token), 200
+    return jsonify({"message": "Invalid credentials"}), 401
