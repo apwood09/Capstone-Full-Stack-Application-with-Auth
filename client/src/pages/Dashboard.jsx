fbf850cd46dae6a9ab4be87eecb3160c08c0dbb9
abbi portwood <apwood09@gmail.com>
2026-06-23 08:05:43 -0400

    added logout button& fix asset grid syntax

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,14 +1,17 @@
 // user sees their data
 // uses api instance to fetch private resources 
 
-import React from 'react';
-import { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from 'react';
 import api from '../api/api'; 
-import AssetCard from '../components/AssetCard'
+import AssetCard from '../components/AssetCard'; 
+import { AuthContext } from '../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
     // intialize local state: store list of assets
     const [assets, setAssets] = useState([]); 
+    const { logout } = useContext(AuthContext); 
+    const navigate = useNavigate(); 
 
     // useEffect triggers logic when components mount 
     useEffect (() => {
@@ -24,13 +27,25 @@ const Dashboard = () => {
         }; 
 
         fetchAssets(); 
-    }, []); // empty dependency array: ensures only runs once on load 
+    }, []); // empty dependency array: ensures only runs once on load
+    
+    const handleLogout = () => {
+        logout(); 
+        navigate('/logout')
+    }; 
 
     return (
         <div>
             <h1 className="text-2xl font-bold mb-6">My Home Assets</h1>
+
+            {/* logout button */}
+            <button onClick={handleLogout} className='bg-red-500 text-white p-2 mb-6 rounded'>
+                Logout 
+            </button>
+
+            {/* asset grid */}
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-                {assets.map(assets => (
+                {assets.map(asset => (
                     <AssetCard key={asset.id} asset={asset} />
                 ))}
             </div>
