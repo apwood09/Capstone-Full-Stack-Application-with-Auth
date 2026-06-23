e051f0c86c9997ab9e74617b96fe0e57728126da
abbi portwood <apwood09@gmail.com>
2026-06-23 08:30:19 -0400

    removed duplicate auth_bp registration & db creates table

--- server/app.py
+++ server/app.py
@@ -4,29 +4,34 @@ from flask_jwt_extended import JWTManager
 from routes.auth import auth_bp
 from routes.resources import res_bp
 from extensions import bcrypt
+from flask_cors import CORS
 
 # initialize Flaksl application
 app = Flask(__name__)
-bcrypt.init_app(app)
+CORS(app)
 
 # CONFIGURATION
 # specifies db files (SQLite
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///homeKeep.db'
 # require to  sign & verify JWT tokens
 app.config['JWT_SECRET_KEY'] = 'super-secret'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # INITIALIZATION
 # connects db instance -> Flask app
 db.init_app(app)
 # sets up JWT (JSON Web Token): handles authentication 
 jwt = JWTManager(app)
+bcrypt.init_app(app)
 
 # BLUEPRINTS
 # registers modular route files -> organize application logic 
 # 'url_prefix' ensures routes are accessed ('/api')
 app.register_blueprint(auth_bp, url_prefix='/api')
 app.register_blueprint(res_bp, url_prefix='/api')
-app.register_blueprint(auth_bp, name='auth_blueprint_unique')
+
+with app.app_context():
+    db.create_all()
 
 # start development server
 if __name__ == '__main__': 
