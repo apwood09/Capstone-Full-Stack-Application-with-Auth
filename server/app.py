575fc32983a57f3fe94f3dcd0c15e9b591f5ce18
abbi portwood <apwood09@gmail.com>
2026-06-24 20:16:35 -0400

    update app.py

--- server/app.py
+++ server/app.py
@@ -34,6 +34,7 @@ app.register_blueprint(auth_bp, url_prefix='/api')
 app.register_blueprint(res_bp, url_prefix='/api')
 
 with app.app_context():
+    db.create_all()
     
 
 # start development server
