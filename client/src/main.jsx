ae0818d7bcf74a74023c4334ffb5cdf72bc0ddab
abbi portwood <apwood09@gmail.com>
2026-06-27 11:45:14 -0400

    notes/comments added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,8 +5,13 @@ import App from './App';
 import { AuthProvider } from './context/AuthContext';
 import './index.css';
 
+// render application tree 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* Wrap <App /> in AuthProvider.
+      uses React Context API to provide authentication state 
+      (current user or login status) to every component in app.
+    */}
     <AuthProvider>
       <App />
     </AuthProvider>
